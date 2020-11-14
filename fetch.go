e086eff75417e856397b00f4b5dfce5e10d46d9d
yu <peacetomori1118@gmail.com>
2020-11-14 22:00:20 +0900

    day9

---
+++ fetch.go
@@ -0,0 +1,28 @@
+// Fetch prints the content found at each specified URL.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if url[0:4] != "http" {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		fmt.Printf("\nStatus: %s", resp.Status)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
