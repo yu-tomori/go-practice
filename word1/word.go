127ed630bb4c69982fb425b2b1d4ebaf0f965f59
yu <peacetomori1118@gmail.com>
2021-01-23 18:15:53 +0900

    day15

---
+++ word1/word.go
@@ -0,0 +1,21 @@
+// Package word provides utilities for word games.
+package word
+
+import "unicode"
+
+// IsPalindrome reports whether s reads the same forward and backward.
+// (Our first attempt.)
+func IsPalindrome(s string) bool {
+	var letters []rune
+	for _, r := range s {
+		if unicode.IsLetter(r) {
+			letters = append(letters, unicode.ToLower(r))
+		}
+	}
+	for i := range s {
+		if letters[i] != letters[len(s)-1-i] {
+			return false
+		}
+	}
+	return true
+}
